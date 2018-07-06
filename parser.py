f8b6c20f7f545231e20f4aabbc21a861e1d9e539
Pablo Torres <ptorres@dcc.uchile.cl>
2018-07-06 12:49:26 -0400

    se agregaron nuevas formas de parsear los datos

--- parser.py
+++ parser.py
@@ -6,20 +6,40 @@ players.readline()
 
 match=open("match.csv","r")
 match.readline()
+tanh=False
+if tanh:
+    x=np.ndarray([50000,113])
+    y=np.ndarray([50000])
+    for i in range(50000):
+        for j in range(10):
+            hero = int(players.readline().split(",")[2])
+            if j<5:
+                x[i, hero] = 1
+            else:
+                x[i, hero] = -1
 
-x=np.ndarray([50000,113])
-y=np.ndarray([50000])
-for i in range(50000):
-    for j in range(10):
-        hero = int(players.readline().split(",")[2])
-        if j<5:
-            x[i, hero] = 1
+        if match.readline().split(",")[9]=="TRUE":
+            y[i]=1
         else:
-            x[i, hero] = -1
+            y[i]=-1
+    np.save("picks",x)
+    np.save("winners",y)
+else:
+    x=np.ndarray([50000,226])
+    y=np.ndarray([50000,2])
+    for i in range(50000):
+        for j in range(10):
+            hero = int(players.readline().split(",")[2])
+            if j<5:
+                x[i, hero] = 1
+            else:
+                x[i, hero+113] = 1
 
-    if match.readline().split(",")[9]=="TRUE":
-        y[i]=1
-    else:
-        y[i]=-1
-np.save("picks",x)
-np.save("winners",y)
+        if match.readline().split(",")[9]=="TRUE":
+            y[i][0]=1
+            y[i][1] = 0
+        else:
+            y[i][0]=0
+            y[i][1] = 1
+    np.save("picks2",x)
+    np.save("winners2",y)
